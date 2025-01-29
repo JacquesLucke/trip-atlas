ab8e412f3282eaad1351af81ac27039e72ce2430
Jacques Lucke <mail@jlucke.com>
2025-01-29 21:51:45 +0100

    initial background map

--- trip-atlas-frontend/src/App.tsx
+++ trip-atlas-frontend/src/App.tsx
@@ -1,5 +1,12 @@
 function App() {
-  return <div className="bg-amber-500">Hello World</div>;
+  return (
+    <div className="absolute top-0 right-0">
+      <div className="bg-amber-500">Hello World</div>
+      <div>
+        <input type="text" className="border-2 border-amber-500" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- trip-atlas-frontend/src/main.tsx
+++ trip-atlas-frontend/src/main.tsx
@@ -2,9 +2,21 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./tailwind.css";
+import * as L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+
+const map = L.map("map-container").setView([52.637778, 13.203611], 14);
+map.attributionControl.setPrefix(
+  "<a href='https://leafletjs.com/'>Leaflet</a>"
+);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
