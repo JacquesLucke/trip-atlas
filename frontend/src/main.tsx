99ff504efcaac02b84d41de96433fa77d10f14ce
Jacques Lucke <mail@jlucke.com>
2025-02-01 00:08:12 +0100

    remove dead code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,6 @@ import "./tailwind.css";
 import * as L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import staticLocations from "./stations_test_data.json";
-import RBush from "rbush";
 
 const useStaticLocations = false;
 const locationsUrl =
@@ -74,90 +73,6 @@ async function main() {
     attribution:
       '<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map);
-
-  interface TileOverlayInfo {}
-
-  const stationsTree = new RBush<StationInfo>();
-
-  stationsTree.load(
-    locations.stations.map((station) => {
-      return {
-        ...station,
-        minX: station.longitude,
-        minY: station.latitude,
-        maxX: station.longitude,
-        maxY: station.latitude,
-      };
-    })
-  );
-
-  const activeTiles = new Map<HTMLElement, TileOverlayInfo>();
-
-  const CustomMapLayer = L.GridLayer.extend({
-    createTile: function (this: L.GridLayer, coords: L.Coords) {
-      const tileSize = this.getTileSize();
-      const corner1 = map.unproject(coords.scaleBy(tileSize), coords.z);
-      const corner2 = map.unproject(
-        coords.add([1, 1]).scaleBy(tileSize),
-        coords.z
-      );
-      const bounds = new L.LatLngBounds([corner1, corner2]);
-
-      const tile = L.DomUtil.create("div", "leaflet-tile");
-      const svgNS = "http://www.w3.org/2000/svg";
-      const svg = document.createElementNS(svgNS, "svg");
-      svg.setAttribute("width", `${tileSize.x}px`);
-      svg.setAttribute("height", `${tileSize.y}px`);
-
-      const circleRadius = 3;
-
-      const paddedBounds = bounds.pad(0.3);
-
-      // const foundStations = stationsTree.search({
-      //   minX: paddedBounds.getWest(),
-      //   minY: paddedBounds.getSouth(),
-      //   maxX: paddedBounds.getEast(),
-      //   maxY: paddedBounds.getNorth(),
-      // });
-
-      // for (const station of foundStations) {
-      //   const pixelPos = map
-      //     .project(new L.LatLng(station.latitude, station.longitude), coords.z)
-      //     .subtract(coords.scaleBy(tileSize));
-      //   if (
-      //     pixelPos.x < -circleRadius ||
-      //     pixelPos.x > tileSize.x + circleRadius ||
-      //     pixelPos.y < -circleRadius ||
-      //     pixelPos.y > tileSize.y + circleRadius
-      //   ) {
-      //     continue;
-      //   }
-
-      //   const time = station.time ?? Number.MAX_VALUE;
-
-      //   const circle = document.createElementNS(svgNS, "circle");
-      //   circle.setAttribute("cx", `${pixelPos.x}`);
-      //   circle.setAttribute("cy", `${pixelPos.y}`);
-      //   circle.setAttribute("r", `${circleRadius}px`);
-      //   circle.setAttribute(
-      //     "fill",
-      //     `hsl(${Math.min(1, time / 10000)}turn, 100%, 50%)`
-      //   );
-      //   circle.setAttribute("opacity", `1.0`);
-
-      //   svg.appendChild(circle);
-      // }
-
-      tile.appendChild(svg);
-      activeTiles.set(tile, {});
-      return tile;
-    },
-  });
-  const customLayer = new CustomMapLayer();
-  customLayer.on("tileunload", (event: L.TileEvent) => {
-    activeTiles.delete(event.tile);
-  });
-  customLayer.addTo(map);
 
   await addOverlayCanvas(map, locations);
 }
